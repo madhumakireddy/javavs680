7e0da5c6fcbb56e0673c8cee8f56773f3450ef83
madhumakireddy <101985626+madhumakireddy@users.noreply.github.com>
2022-05-20 22:11:52 -0400

    Add files via upload

---
+++ HW14/src/edu/umb/cs680/HW14/ApfsElement.java
@@ -0,0 +1,31 @@
+package edu.umb.cs680.HW14;
+
+import java.time.LocalDateTime;
+
+public abstract class ApfsElement extends FSElement
+{	
+	private String ownerName;
+	private LocalDateTime lastModified;
+	
+	public ApfsElement(ApfsDirectory parent, String name, int size, LocalDateTime creationTime, String ownerName,LocalDateTime lastModified) 
+	{
+		super(parent, name, size, creationTime);
+		this.ownerName = ownerName;
+		this.lastModified = lastModified;
+	}
+
+	public String getOwnerName() 
+	{
+		return ownerName;
+	}
+
+	public LocalDateTime getLastModified() 
+	{
+		return lastModified;
+	}
+
+	public static void main(String[]args)
+	{
+		System.out.println("The apfs element class has been sucessfully created");
+	}
+}
---
+++ HW14/src/edu/umb/cs680/HW14/FileSystem.java
@@ -0,0 +1,56 @@
+package edu.umb.cs680.HW14;
+
+public abstract class FileSystem 
+{	
+	protected String fileSystemName;
+	protected  int capacity;
+	protected  int id;
+	private FSElement rootDirectory;
+	
+	protected abstract FSElement createDefaultRoot();
+	
+	public FSElement initFileSystem(String name, int capacity) 
+	{
+		this.fileSystemName = name;
+		this.capacity = capacity;
+		FSElement root = createDefaultRoot();
+		if (root.isDirectory() && capacity > root.getSize()) 
+		{
+			setRootDir(root);
+			this.id = root.hashCode();
+			return root;
+		}
+		else
+			return null;
+	}
+
+	public String getFileSystemName() 
+	{
+		return fileSystemName;
+	}
+
+	public int getCapacity() 
+	{
+		return capacity;
+	}
+
+	public int getId() 
+	{
+		return id;
+	}
+	
+	public FSElement getRootDir() 
+	{
+		return this.rootDirectory;
+	}
+
+	private void setRootDir(FSElement rootDirectory) 
+	{
+		this.rootDirectory = rootDirectory;
+	}
+
+	public static void main(String[]args)
+	{
+		System.out.println("The file system class has been sucessfully created");
+	}
+}
